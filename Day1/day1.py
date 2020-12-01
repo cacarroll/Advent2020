7ab49c32f10a9bbc1c4fb6231e776ecc5e45246b
Chad Carroll <ccarroll@microsoft.com>
2020-12-01 10:51:09 -0500

    add unit tests

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,25 +1,16 @@
-def add(x, y, z=0):
-    if x + y + z == 2020:
-        print(f"{x} plus {y} plus {z} equals 2020")
-        multiply(x, y, z)
-
-
-def multiply(x, y, z):
-    if z == 0:
-        result = x * y
-    else:
-        result =  x * y * z
-    print(result)
-
+from calc import add, multiply
 
 with open('input.txt') as file:
     entries = [int(line.rstrip()) for line in file]
     for x in entries:
         for y in entries:
-            add(x, y)
+            result = add(x, y)
+            if result == 2020:
+                print(f"{x} plus {y} = 2020")
+                multiply(x, y)
             for z in entries:
-                add(x, y, z)
-
-
-
-
+                result = add(x, y, z)
+                if result == 2020:
+                    print(f"{x} plus {y} plus {z} = 2020")
+                    result = multiply(x, y, z)
+                    print(f"Multiplied equals {result}")
--- Day1/test_day1.py
+++ Day1/test_day1.py
@@ -1,10 +1,25 @@
 import unittest
-import day1
+import calc
 
 class TestDay1(unittest.TestCase):
+    def test_add(self):
+        year = 2020
+        result = calc.add(10, 2010)
+        self.assertEqual(result, year)
+
+        result = calc.add(220, 1800)
+        self.assertEqual(result, year)
+
+        result = calc.add(420, 800)
+        self.assertNotEqual(result,year)
+
     def test_multiply(self):
-        result = day1.multiply(10, 20)
-        self.assertEqual(result, 2020)
+        result = calc.multiply(10, 200)
+        self.assertEqual(result, 2000)
+
+        result = calc.multiply(50, 10)
+        self.assertEqual(result, 500)
+        self.assertNotEqual(result, 501)
 
 
 if __name__ == '__main__':

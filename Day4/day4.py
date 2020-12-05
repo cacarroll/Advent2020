548a1e8cfa4ff720e61f223e9517c74e1126a356
Chad Carroll <ccarroll@microsoft.com>
2020-12-04 20:58:20 -0500

    challenge 1 assert tests

--- Day4/day4.py
+++ Day4/day4.py
@@ -26,3 +26,14 @@ if is_passport_valid(currentPassport):
     ValidCount += 1
 
 print(ValidCount)
+
+
+pp1 = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+pp2 = "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929"
+pp3 = "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"
+pp4 = "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
+
+assert is_passport_valid(pp1) == True
+assert is_passport_valid(pp2) == False
+assert is_passport_valid(pp3) == True
+assert is_passport_valid(pp4) == False
